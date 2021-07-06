eb4db3c6825a62113db021136eb5509004781322
Morgan Gallant <morgan@morgangallant.com>
2021-07-06 19:16:36 -0400

    add ~m
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 const Index = () => {
   return (
-    <p>
-      It&apos;s really stupid that I need a headless browser to do a GET request in
-      2021. Why can&apos;t we go back to the good ol&apos; days of pure HTML / CSS
-      websites? It would be a whole hell of a lot cleaner.
-    </p>
+    <div>
+      <p>
+        It&apos;s really stupid that I need a headless browser to do a GET
+        request in 2021. Why can&apos;t we go back to the good ol&apos; days of
+        pure HTML / CSS websites? It would be a whole hell of a lot cleaner.
+      </p>
+      <br />
+      <p>~ M</p>
+    </div>
   );
-}
+};
 
 export default Index;
