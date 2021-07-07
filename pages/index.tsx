3e457473d980566707ab98633e3e9c4a77538744
Morgan Gallant <morgan@morgangallant.com>
2021-07-07 00:06:18 -0400

    irony
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ const Index = () => {
         request in 2021. Why can&apos;t we go back to the good ol&apos; days of
         pure HTML / CSS websites? It would be a whole hell of a lot cleaner.
       </p>
+      <p>And yes, I do get the irony that this website itself is a bunch of JavaScript. Gotta pick your battles.</p>
       <p>~ M</p>
     </div>
   );
